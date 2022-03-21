d51096c51ac3e14e0d7cea937d9248daf5e0c35f
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2022-03-21 12:53:32 +0300

    Add multithreading flag

--- CloudDownloader.py
+++ CloudDownloader.py
@@ -71,7 +71,7 @@ def recv_body(s, start, end_index):
     return res
 
 
-def get_partial(url, cred, offset, end):
+def get_partial(url, cred, offset, end, byte_range):
     """
     Requests a partial txt file and returns the start-end portion of the body
     """
@@ -89,24 +89,30 @@ def get_partial(url, cred, offset, end):
         )
         s.sendall(bytes(req, encoding="ascii"))
         res = recv_header(s)
+        print("Connected to %s" % url) # print selected url, might be out of order because of threading
         header_res, body_start = split_header(res)
         header = header_res.decode()
         body_res = recv_body(s, body_start, get_content_length(header))
+        print("Downloaded bytes %r to %r (size = %r)" % (byte_range[0]+offset, byte_range[1], end-offset))
     return body_res[offset:end]
 
 
-def get_all_partials(data):
+def get_all_partials(data, multi):
     """
     Requests all partial txt files in the given data returns the resulting text 
     file name, size, and partials
     """
     lines = data.rstrip().split("\n")
+    print("File size is %s Bytes" % lines[1]) # print file size
+    print("Index file is downloaded") # printed here according to the given example
     partial_lines = lines[2:]
     partials = [partial_lines[n:n+3] for n in range(0, len(partial_lines), 3)]
+    print("There are %r servers in the index" % len(partials)) # print number of partial urls
     urls = []
     creds = []
     offsets = []
     ends = []
+    ranges = []
     prev_end = 0
     for partial in partials:
         start, end = partial[2].split("-", 1)
@@ -115,10 +121,13 @@ def get_all_partials(data):
         creds.append(partial[1])
         offsets.append(prev_end-start+1)
         ends.append(end-start+1)
+        ranges.append((start, end))
         prev_end = end
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        result = executor.map(get_partial, urls, creds, offsets, ends)
+    if multi:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            result = executor.map(get_partial, urls, creds, offsets, ends, ranges)
+    else:
+        result = map(get_partial, urls, creds, offsets, ends, ranges)
 
     return (lines[0], lines[1], result)
 
@@ -126,6 +135,7 @@ def get_all_partials(data):
 def main(args):
     """ Program start """
     url = vars(args)["index_file"]
+    print("URL of the index file: %s" % url) # print the url
     HOST, PATH = url.split("/", 1)  # use urllib.parse for better parsing
     PATH = "/" + PATH
     credentials = str(base64.b64encode(
@@ -145,8 +155,9 @@ def main(args):
         header = header_res.decode()
         body_res = recv_body(s, body_start, get_content_length(header))
     body = body_res.decode()
-    filename, filesize, data = get_all_partials(body)
+    filename, filesize, data = get_all_partials(body, vars(args)["multi"])
     result_string = (b"".join(data)).decode()
+    print("Download of the file is complete (size = %s)" % filesize)
     with open(filename, "w+") as result_file:
         result_file.write(result_string)
 
@@ -155,12 +166,15 @@ if __name__ == "__main__":
     """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
 
-    # Required positional argument
+    # Required positional arguments
     parser.add_argument(
         "index_file", help="The URL of the index that includes the list of partial file locations and their authentication information")
 
     parser.add_argument(
         "username:password", help="The authentication information of the index server, in the structure <username>:<password>")
+
+    # Multithreading flag
+    parser.add_argument("-m", "--multi", action="store_true", default=False)
 
     # Specify output of "--version"
     parser.add_argument(
